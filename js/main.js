5eb88d303b918c1576370e8c67ff9aecbb37c0b2
sharmandhruvpl <sharmandhruv.pl@gmail.com>
2025-08-04 15:50:41 +0200

    Fresh website files

---
+++ js/main.js
@@ -0,0 +1,157 @@
+$(document).ready(function() {
+
+    // Speaker Data
+    const speakers = [
+        {
+            name: "Prof. Alfred Leitenstorfer",
+            topic: "Measuring the quantum vacuum",
+            image: "images/speakers/leitenstorfer.jpg"
+        },
+        {
+            name: "Prof. Markus Oberthaler",
+            topic: "Cosmology in the laboratory",
+            image: "images/speakers/oberthaler.jpg"
+        },
+        {
+            name: "Dr. W. Giare",
+            topic: "Cosmological tensions",
+            image: "images/speakers/giare.jpg"
+        },
+        {
+            name: "Tina Hertel",
+            affiliation: "ETH Zurich",
+            topic: "Measuring vacuum correlations",
+            image: "images/speakers/herter.jpg"
+        },
+        {
+            name: "Prof. Matthias Fink",
+            topic: "Wave Control",
+            image: "images/speakers/fink.jpeg"
+        },
+        {
+            name: "Prof. Germain Rousseaux",
+            topic: "Hydrodynamic Analogues",
+            image: "images/speakers/rousseaux.jpg"
+        },
+        {
+            name: "Prof. Stefan Rotter",
+            topic: "Wave Phenomena",
+            image: "images/speakers/rotter.jpg"
+        }
+    ];
+
+    // Organizer Data
+    const organizers = [
+        {
+            name: "Prof. Alexandre Tkatchenko",
+            affiliation: "University of Luxembourg"
+        },
+        {
+            name: "Prof. Ulf Leonhardt",
+            affiliation: "University of Luxembourg"
+        }
+    ];
+
+    // --- Dynamic Card Generation ---
+
+    // Generate and inject Speaker Cards with layout fix
+    const speakerList = $('.speaker-list');
+    speakers.forEach((speaker, index) => {
+        // Build the speaker card HTML
+        const speakerCard = `
+            <div class="col-md-4 col-sm-6 reveal-element">
+                <div class="speaker-card">
+                    <img src="${speaker.image}" alt="${speaker.name}" class="img-responsive">
+                    <div class="speaker-info">
+                        <h4>${speaker.name}</h4>
+                        ${speaker.affiliation ? `<h5>${speaker.affiliation}</h5>` : ''}
+                        <p>${speaker.topic}</p>
+                    </div>
+                </div>
+            </div>`;
+        speakerList.append(speakerCard);
+
+        // *** THIS IS THE FIX ***
+        // After every 3rd speaker, add a clearing div to fix alignment
+        if ((index + 1) % 3 === 0) {
+            speakerList.append('<div class="clearfix visible-md-block visible-lg-block"></div>');
+        }
+    });
+
+    // Generate and inject Organizer Cards
+    const organizerList = $('.organizer-list');
+    organizers.forEach(organizer => {
+        const organizerCard = `
+            <div class="col-md-4 col-sm-6 reveal-element">
+                <div class="organizer-card">
+                    <h4>${organizer.name}</h4>
+                    <p>${organizer.affiliation}</p>
+                </div>
+            </div>`;
+        organizerList.append(organizerCard);
+    });
+
+
+    // --- Basic Website Functionality (Preloader, Slider, Menu, etc.) ---
+
+    // Preloader
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1000);
+
+    // Header Slider
+    if ($('.header-slider').length) {
+        $('.header-slider .slides').imagesLoaded(function() {
+            $('.header-slider').flexslider({
+                animation: "fade",
+                controlNav: false,
+                directionNav: false,
+                slideshowSpeed: 4000,
+                animationSpeed: 1000,
+                pauseOnAction: false,
+                smoothHeight: true
+            });
+        });
+    }
+
+    // Mobile Menu
+    $('.navbar-toggle').on('click', function() {
+        $('#menu').toggleClass('open-menu');
+    });
+
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').on('click', function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                if ($('#menu').hasClass('open-menu')) {
+                    $('#menu').removeClass('open-menu');
+                }
+                return false;
+            }
+        }
+    });
+
+    // Animate on scroll
+    function revealOnScroll() {
+        var $elems = $('.reveal-element');
+        var winheight = $(window).height();
+        $elems.each(function() {
+            var $this = $(this);
+            var pos = $this.offset().top;
+            if (pos < $(window).scrollTop() + winheight - 50) {
+                $this.addClass('revealed');
+            }
+        });
+    }
+
+    $(window).scroll(function() {
+        revealOnScroll();
+    });
+    revealOnScroll(); // Initial check
+
+});
